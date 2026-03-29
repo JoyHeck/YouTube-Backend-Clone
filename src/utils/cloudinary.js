644b0500351de329e1c6a1541f4302c725ecaf04
Joy Kharinta <kharintajoy2343@gmail.com>
2026-03-30 00:38:34 +0530

    added Cloudinary and Multer for file management

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is Uploaded onClooudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temp file as the upload operation get failed
+    return null;
+  }
+};
+export { uploadOnCloudinary };
